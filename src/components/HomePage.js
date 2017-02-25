7d57147de7f0bf118853cf372556b8362a7c15eb
akataz <tyrus.brenc@gmail.com>
2017-02-25 15:51:07 -0800

    add react bootstrap with navbar component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router';
+//import {Link} from 'react-router';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Tyrus Brenc</h1>
-
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
-    </div>
-  );
-};
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <h1>Tyrus Brenc</h1>
+//
+//       <h2>Get Started</h2>
+//       <ol>
+//         <li>Review the <Link to="fuel-savings">demo app</Link></li>
+//         <li>Remove the demo and start coding: npm run remove-demo</li>
+//       </ol>
+//     </div>
+//   );
+// };
+const HomePage = (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem eventKey={1} href="#">Link</NavItem>
+      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
+);
 
 export default HomePage;
