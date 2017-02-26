6a1b32c1197fb1ce1e9bf365443ca4814a700c04
akataz <tyrus.brenc@gmail.com>
2017-02-25 17:51:07 -0800

    remove demo app from project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,7 @@ import React from 'react';
 //import {Link} from 'react-router';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
-// const HomePage = () => {
-//   return (
-//     <div>
-//       <h1>Tyrus Brenc</h1>
-//
-//       <h2>Get Started</h2>
-//       <ol>
-//         <li>Review the <Link to="fuel-savings">demo app</Link></li>
-//         <li>Remove the demo and start coding: npm run remove-demo</li>
-//       </ol>
-//     </div>
-//   );
-// };
-const HomePage = () => {
+const NavBar = () => {
 
   return (
     <Navbar>
@@ -26,17 +13,21 @@ const HomePage = () => {
       </Navbar.Header>
       <Nav>
         <NavItem eventKey={1} href="#">Projects</NavItem>
-        <NavItem eventKey={2} href="#">Github</NavItem>
+        <NavItem eventKey={2} href="https://www.github.com/akataz">Github</NavItem>
         <NavItem eventKey={4} href="#">About</NavItem>
-        <NavDropdown eventKey={3} title="Personal" id="basic-nav-dropdown">
+        <NavDropdown pullRight eventKey={3} title="Personal" id="basic-nav-dropdown">
           <MenuItem eventKey={3.1}>Recipes</MenuItem>
           <MenuItem divider />
-          <MenuItem eventKey={3.2}>JobHunter</MenuItem>
+          <MenuItem eventKey={3.2}>JobHunter9000</MenuItem>
         </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
       </Nav>
     </Navbar>
   )
 };
 
 
-export default HomePage;
+export default NavBar;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
-import fuelSavings from './fuelSavingsReducer';
+//import fuelSavings from './fuelSavingsReducer';
 import {routerReducer} from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  fuelSavings,
   routing: routerReducer
 });
 
