e1a6e8986356d39e1dfb837e608b7143c3aba776
akataz <tyrus.brenc@gmail.com>
2017-03-01 14:11:30 -0800

    projects now separate component

---
+++ src/components/projects.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import "../lib/css/landing-page.css";
+
+const Projects = () => {
+  return (
+    <div>
+        <div className="content-section-a">
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-5 col-sm-6">
+                        <hr className="section-heading-spacer" />
+                        <div className="clearfix"></div>
+                        <h2 className="section-heading">ApexSwipe:<br /><h3>Hardware integrated realtime data sharing app</h3></h2>
+                        <p className="lead">Apex Swipe is a native mobile application that allows users to build virtual "decks" to get their friends opinions using swipe touch technology. It is written in React Native with Redux for both iOS and Android, and features include hardware integration via the mobile device's camera, Yelp API queries for restaurant data, and a Heroku and AWS deployed Node/Express/MongoDB back-end for API calls and user data storage. Our team utilized Test Driven Development via Mocha and Chai, as well as AGILE to keep the development process focused and efficient.</p>
+                    </div>
+                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                        <img className="img-responsive" src="../lib/img/apexSwipe.jpeg" alt=""/>
+                    </div>
+                </div>
+
+            </div>
+            {/*//  /.container -->*/}
+
+        </div>
+        {/*//  /.content-section-a -->*/}
+
+        <div className="content-section-b">
+
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                        <hr className="section-heading-spacer"/>
+                        <div className="clearfix"></div>
+                        <h2 className="section-heading">Share Anything 2.0</h2>
+                        <p className="lead">A rental service riding on React and Redux, Share Anything 2.0 is inspired by AirBnB and incorporates such features as real time product chat and integrated payment solutions.</p>
+                    </div>
+                    <div className="col-lg-5 col-sm-pull-6  col-sm-6">
+                        <img className="img-responsive" src="../lib/img/shareAnything.png" alt=""/>
+                    </div>
+                </div>
+
+            </div>
+            {/*//  /.container -->*/}
+
+        </div>
+        {/*//  /.content-section-b -->*/}
+
+        <div className="content-section-a">
+
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-5 col-sm-6">
+                        <hr className="section-heading-spacer"/>
+                        <div className="clearfix"></div>
+                        <h2 className="section-heading">Omgeo:<br />Geotagging Domain Registrar</h2>
+                        <p className="lead">Create longitude and latitude based tags registered in a central repository</p>
+                    </div>
+                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                        <img className="img-responsive" src="../lib/img/omgeo.png" alt=""/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects;
--- src/components/template.js
+++ src/components/template.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Footer from "./Footer.js"
 import "../lib/css/landing-page.css";
 import NavBar from "./NavBar.js"
+import Projects from "./projects.js"
 
 
 const Template = () => {
@@ -44,71 +45,7 @@ const Template = () => {
         {/*//  Page Content -->*/}
 
         <a  name="services"></a>
-        <div className="content-section-a">
-
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-5 col-sm-6">
-                        <hr className="section-heading-spacer" />
-                        <div className="clearfix"></div>
-                        <h2 className="section-heading">ApexSwipe:<br /><h3>Hardware integrated realtime data sharing app</h3></h2>
-                        <p className="lead">Apex Swipe is a native mobile application that allows users to build virtual "decks" to get their friends opinions using swipe touch technology. It is written in React Native with Redux for both iOS and Android, and features include hardware integration via the mobile device's camera, Yelp API queries for restaurant data, and a Heroku and AWS deployed Node/Express/MongoDB back-end for API calls and user data storage. Our team utilized Test Driven Development via Mocha and Chai, as well as AGILE to keep the development process focused and efficient.</p>
-                    </div>
-                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                        <img className="img-responsive" src="../lib/img/apexSwipe.jpeg" alt=""/>
-                    </div>
-                </div>
-
-            </div>
-            {/*//  /.container -->*/}
-
-        </div>
-        {/*//  /.content-section-a -->*/}
-
-        <div className="content-section-b">
-
-            <div className="container">
-
-                <div className="row">
-                    <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
-                        <hr className="section-heading-spacer"/>
-                        <div className="clearfix"></div>
-                        <h2 className="section-heading">Share Anything 2.0</h2>
-                        <p className="lead">A rental service riding on React and Redux, Share Anything 2.0 is inspired by AirBnB and incorporates such features as real time product chat and integrated payment solutions.</p>
-                    </div>
-                    <div className="col-lg-5 col-sm-pull-6  col-sm-6">
-                        <img className="img-responsive" src="../lib/img/shareAnything.png" alt=""/>
-                    </div>
-                </div>
-
-            </div>
-            {/*//  /.container -->*/}
-
-        </div>
-        {/*//  /.content-section-b -->*/}
-
-        <div className="content-section-a">
-
-            <div className="container">
-
-                <div className="row">
-                    <div className="col-lg-5 col-sm-6">
-                        <hr className="section-heading-spacer"/>
-                        <div className="clearfix"></div>
-                        <h2 className="section-heading">Omgeo:<br />Geotagging Domain Registrar</h2>
-                        <p className="lead">Create longitude and latitude based tags registered in a central repository</p>
-                    </div>
-                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                        <img className="img-responsive" src="../lib/img/omgeo.png" alt=""/>
-                    </div>
-                </div>
-
-            </div>
-            {/*//  /.container -->*/}
-
-        </div>
-        {/*//  /.content-section-a -->*/}
-
+        <Projects />
         <a  name="contact"></a>
         <div className="banner">
 
@@ -116,7 +53,7 @@ const Template = () => {
 
                 <div className="row">
                     <div className="col-lg-6">
-                        <h2>Connect to me!</h2>
+                        <h2>Connect with me!</h2>
                     </div>
                     <div className="col-lg-6">
                         <ul className="list-inline banner-social-buttons">
