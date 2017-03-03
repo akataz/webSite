fa1f0a051dfb5ac398e99ba683af33e25933cf56
akataz <tyrus.brenc@gmail.com>
2017-03-02 21:26:56 -0800

    add procfile for heroku deploy

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Footer from "./Footer.js"
+import Header from "./NavBar.js"
+
 const NotFoundPage = () => {
   return (
     <div>
+      <Header />
       <h4>
         404 Page Not Found
       </h4>
       <Link to="/"> Go back to homepage </Link>
+      <Footer />
     </div>
   );
 };
